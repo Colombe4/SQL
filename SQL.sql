2da78db516a14ccff2ab71d7c823ba86fd4412eb
Colombe4 <Colsaiz@yahoo.fr>
2020-01-30 14:20:05 +0100

    ajout questions 7 et 8

--- SQL.sql
+++ SQL.sql
@@ -1,2 +1,13 @@
 # test
 use dataia_Nancy;
+SELECT anciennete, sinistre, COUNT(*)   
+    FROM Dataia_nancy
+    GROUP BY anciennete, sinistre;
+
+
+SELECT  
+ROUND( ((count(anciennete) + COUNT(sinistre) ) /  count(*) ) * 100, 2) 
+AS pourcentage 
+FROM dataia_nancy 
+GROUP BY anciennete, sinistre
+ORDER BY pourcentage DESC;
